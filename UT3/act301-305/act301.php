245c9e8fc5e3d2d071752c335a0e3ed0eb03331c
jgrijalazp1 <jgrijalazp1@educacion.navarra.es>
2026-09-22 13:47:35 +0200

    act301 terminada

--- UT3/act301-305/act301.php
+++ UT3/act301-305/act301.php
@@ -19,10 +19,40 @@ $diasDesdeEnvio = 5;
 $color = 'gray';
 $mensaje = 'Estado desconocido.';
 $notaAdicional = '';
+$alerta ="";
+
+switch($estado){
+    case 'ENVIADO': 
+        $mensaje = "Enviado"; 
+        $color = 'yellow';
+        if($diasDesdeEnvio > 3){$alerta= "Ya han pasado mas de 3 dias desde el envio";}
+        break;
+        
+    case 'PENDIENTE': 
+        $mensaje = "Pendiente";
+        $color = 'brown';
+        break;
+    case 'ENTREGADO':
+        $mensaje = "Entregado";
+        $color = 'green';
+        break;
+    case 'CANCELADO': 
+        $mensaje = "brown";
+        $color = 'red';
+        break;
+    default: $mensaje = 'Estado desconocido';
+}
+
+
+
 // Renderizado de la etiqueta HTML
-echo "<div style='padding: 10px; background-color: {$color}; color: white; border-radius: 5px;'>";
+echo "<div style='padding: 10px; background-color: {$color}; color: white; border-radius: 5px; font-size: 20px'>";
 echo "<strong>Estado:</strong> {$mensaje}{$notaAdicional}";
 echo "</div>";
-
+if($alerta){ 
+    echo"<script>
+                alert('Ya han pasado mas de 3 dias desde el envio');
+         </script>";
+    }
 
 ?>
